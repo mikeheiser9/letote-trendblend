812fc690f95de2877715a174154516adf236c8eb
mikeheiser9 <mikeheiser9@gmail.com>
2019-06-06 18:46:45 -0400

    animations done

---
+++ app/public/js/app.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    console.log( "Im here and ready!" );
+    // $('#popoverContent').html('<div class="row"><div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6"><div class="inner-img"><img class="dress-img" src="https://d2zk4u3pjs0oai.cloudfront.net/assets/yellow-dress-laydown-2408ab6c320f7b43a46a896c47ace1fa.png"></div></div><div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6"><div class="pop-text-cont"><h2 class="top-pop-txt">Want to save 30% on your new look?</h2><a class="code-btn" href="#"><button class="get-offer-btn" id="getOfferBtn">Yes! Get Code</button></a><a class="exit-link" href="#"><p class="btm-pop-txt">No thank you, keep shopping</p></a></div></div></row>')
+
+    $('#getOfferBtn').on('click',function(){
+        $('#')
+    });
+
+
+});
